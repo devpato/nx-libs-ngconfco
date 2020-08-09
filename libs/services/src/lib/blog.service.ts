b5af0c6f81936bf5de8a5a83c30195b4d29be074
devpato <pevargas2730@gmail.com>
2020-08-09 19:14:23 -0400

    feat(temp): lib services

---
+++ libs/services/src/lib/blog.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from '@ngconf/post';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogService {
+  BASE_URL = 'https://ngconf-52e39.firebaseio.com/';
+  BLOG_EP = 'posts.json';
+
+  constructor(private http: HttpClient) { }
+
+  posts$ = this.http.get<Post[]>(this.BASE_URL + this.BLOG_EP);
+
+  getPost(id: String): Observable<Post> {
+    return this.http.get<Post>(`${this.BASE_URL}/posts/${id}.json`);
+  }
+}
