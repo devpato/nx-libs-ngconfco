629aba87a4de3b4e71b917cedc9241e32fce9d63
devpato <pevargas2730@gmail.com>
2020-08-13 01:10:08 -0400

    fix interface post

--- libs/post/src/lib/post.ts
+++ libs/post/src/lib/post.ts
@@ -1,7 +1,7 @@
 export interface Post {
-    id: string,
-    title: string,
-    author: string,
-    description: string,
-    imageUrl: string
+    id: string;
+    title: string;
+    author: string;
+    description: string;
+    imageUrl: string;
 }
